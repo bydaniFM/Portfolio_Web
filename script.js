ab1fd049522a1a20a1212d702a8d5c0805bdb96f
bydaniFM <daniFM333@gmail.com>
2016-12-17 11:34:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrar(n){
+	document.getElementById("submenu"+n).style.display="block";
+}
+function ocultar(){
+	document.getElementById("submenu1").style.display="none";
+	document.getElementById("submenu2").style.display="none";
+}
